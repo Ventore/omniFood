4a54d8f980b5d98b10673670d9d2a64c62a55e07
Bohdan Basov <basovbohdan@gmail.com>
2017-03-15 13:48:25 +0200

    Add animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+  // Sticky nav bar with waypoints plugin
   $('.js--section-features').waypoint(function(direction) {
     if (direction === "down") {
       $('nav').addClass("sticky")
@@ -9,7 +9,7 @@ $(document).ready(function() {
   }, {
     offset: "60px;"
   })
-
+  // Scroll To on click buttons
   $('.js--scroll-to-plans').click(function() {
     console.log("click");
     $('html, body').animate({scrollTop: $('.js--section-plans').offset().top -60}, 1000);
@@ -21,17 +21,41 @@ $(document).ready(function() {
   })
   //Navigation scroll
   $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top -60
-        }, 1000);
-        return false;
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top -60
+          }, 1000);
+          return false;
+        }
       }
-    }
+    });
   });
-});
+  // Animation on scroll
+  $('.js--section-features').waypoint(function(direction) {
+    $(".js--waypoint-1").addClass('animated fadeIn');
+  }, {
+    offset: '60px;'
+  });
+
+  $('.js--section-steps').waypoint(function(direction) {
+    $(".js--waypoint-2").addClass('animated fadeInUp');
+  }, {
+    offset: '60px;'
+  });
+
+  $('.js--section-cities').waypoint(function(direction) {
+    $(".js--waypoint-3").addClass('animated fadeIn');
+  }, {
+    offset: '60px;'
+  });
+
+  $('.js--section-plans').waypoint(function(direction) {
+    $(".js--waypoint-4").addClass('animated pulse');
+  }, {
+    offset: '60px;'
+  });
 });
